9c88eff24483e40c1663570639139ce99d1b8d99
AyushSriv007 <36125267+AyushSriv007@users.noreply.github.com>
2025-06-18 10:50:10 +0530

    Fixed routine skin care bottle issue

--- src/components/routine-steps/RoutineSteps.jsx
+++ src/components/routine-steps/RoutineSteps.jsx
@@ -9,25 +9,36 @@ const RoutineSection = () => {
 
   const handleMouseEnter = (type) => setHoverType(type);
   const handleMouseLeave = () => setHoverType('');
-
   const handleClick = (type) => {
     if (type === '5') navigate('/collections/5-step-korean-skincare-sets');
-    if (type === '10') navigate('/collections/10-step-korean-skin-care-routine-sets');
+    else if (type === '10') navigate('/collections/10-step-korean-skin-care-routine-sets');
   };
 
   return (
-    <div className={`routine-section-container ${hoverType === '5' ? 'hover-5' : hoverType === '10' ? 'hover-10' : ''}`}>
+    <div className={`routine-section-container ${hoverType ? `hover-${hoverType}` : ''}`}>
       <div className="routine-headline">
         Get started with our curated Korean Skincare Routine Sets!
       </div>
 
       <div className="routine-steps-images-container">
-        <BottlesSVG className="step-bottles-container" />
+        <div
+          className="bottles-hover-area"
+          onMouseEnter={() => handleMouseEnter('5')}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => handleClick('5')}
+        />
+        <div
+          className="bottles-hover-area right"
+          onMouseEnter={() => handleMouseEnter('10')}
+          onMouseLeave={handleMouseLeave}
+          onClick={() => handleClick('10')}
+        />
+        <BottlesSVG />
       </div>
 
       <div className="routine-steps-links-container">
         <span
-          className="routine-step-link"
+          className=" banner__fullwidth--content__btn primary__btn routine-step-link"
           onMouseEnter={() => handleMouseEnter('5')}
           onMouseLeave={handleMouseLeave}
           onClick={() => handleClick('5')}
@@ -35,7 +46,7 @@ const RoutineSection = () => {
           5 Step
         </span>
         <span
-          className="routine-step-link"
+          className="banner__fullwidth--content__btn primary__btn routine-step-link"
           onMouseEnter={() => handleMouseEnter('10')}
           onMouseLeave={handleMouseLeave}
           onClick={() => handleClick('10')}
